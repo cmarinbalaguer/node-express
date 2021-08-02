8a69eb32e98f96924f30821ad5f3563d921581c4
Carlos Marín Balaguer <cmarin@atsistemas.com>
2021-08-02 13:49:47 +0200

    quitar carpeta dist e imagenes

--- usuarios-back-ts/dist/config.js
+++
@@ -1,6 +0,0 @@
-const DB_HOSTNAME = '127.0.0.1';
-const DB_PORT = '27017';
-const DB_NAME = 'users';
-const DB_URL = `mongodb://${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}`;
-module.exports = DB_URL;
-//# sourceMappingURL=config.js.map
